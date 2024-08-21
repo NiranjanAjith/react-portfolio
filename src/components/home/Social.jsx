7beb9e77e039f90f6a25fb11d3791be7cf8c4eb5
NiranjanAjith <NiranjanAjithKumar@gmail.com>
2024-08-21 23:28:17 +0530

    fixed firebase interference

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -4,10 +4,10 @@ import { FiGithub, FiLinkedin} from "react-icons/fi";
 const Social = () => {
     return (
         <div className="home__social">
-            <a href="https://www.github.com/NiranjanAjith" className="home__social-icon" target="_blank">
+            <a href="https://www.github.com/NiranjanAjith" className="home__social-icon" target="_blank" rel='noopener noreferrer'>
                 <FiGithub />
             </a>
-            <a href="https://www.linkedin.com/in/niranjan-ajith" className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.com/in/niranjan-ajith" className="home__social-icon" target="_blank" rel='noopener noreferrer'>
                 <FiLinkedin />
             </a>
         </div> 
--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import './scrollup.css';
 import { HiOutlineArrowSmUp } from "react-icons/hi";
 
 const ScrollUp = () => {
-    window.addEventListener("scroll", function () {
-        const scrollUp = document.querySelector(".scrollup");
-        if(this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    });
-  
-    return (
-    <a href="#" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon"/></a>
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector(".scrollup");
+      if (window.scrollY >= 560) {
+        scrollUp.classList.add("show-scroll");
+      } else {
+        scrollUp.classList.remove("show-scroll");
+      }
+    };
+
+    // Add event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
+
+  return (
+    <a href="#home" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon"/></a>
   );
 }
 
